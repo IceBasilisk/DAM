65268bc44408f3064133cfe21b7cf5226dab666c
IceBasilisk <luis.simoes2001@hotmail.com>
2026-04-15 16:25:25 +0100

    Projeto TP3 iniciado

---
+++ TP3/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+
+    implementation(project(":annotations"))
+
+    kapt(project(":processor"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
