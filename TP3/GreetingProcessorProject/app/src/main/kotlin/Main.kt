1a4c977d6bcbc0cd1663e868f3c48ffb78e76445
IceBasilisk <luis.simoes2001@hotmail.com>
2026-04-29 17:41:47 +0100

    TP3 reorganização
    Reorganização do trabalho em pastas GreetingProcessorProject e WeatherCompose, restruturação do Cool Weather App com Jetpack Compose

---
+++ TP3/GreetingProcessorProject/app/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import `unnamed package`.MyClassWrapper
+
+
+fun main() {
+    val myClass = MyClass()
+    val wrappedMyClass = MyClassWrapper(myClass) // Use the wrapper class
+
+    wrappedMyClass.sayHello()
+    wrappedMyClass.compute()
+}
---
+++ TP3/WeatherCompose/app/src/main/java/dam_47514/weathercompose/ui/WeatherScreen.kt
@@ -0,0 +1,157 @@
+package dam_47514.weathercompose.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import dam_47514.weathercompose.viewmodel.WeatherViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dam_47514.weathercompose.data.WMO_WeatherCode
+import dam_47514.weathercompose.data.getWeatherCodeMap
+
+@Preview
+@Composable
+fun WeatherUI(weatherViewModel: WeatherViewModel = viewModel()) {
+    val weatherUIState by weatherViewModel.uiState.collectAsState()
+    val latitude = weatherUIState.latitude
+    val longitude = weatherUIState.longitude
+    val temperature = weatherUIState.temperature
+    val windSpeed = weatherUIState.windspeed
+    val windDirection = weatherUIState.winddirection
+    val weathercode = weatherUIState.weathercode
+    val seaLevelPressure = weatherUIState.seaLevelPressure
+    val time = weatherUIState.time
+    val configuration = LocalConfiguration.current
+    val day = true // Must change this in the future
+    val mapt = getWeatherCodeMap()
+    val wCode = mapt.get(weathercode)
+    val wImage = when (wCode) {
+        WMO_WeatherCode.CLEAR_SKY,
+        WMO_WeatherCode.MAINLY_CLEAR,
+        WMO_WeatherCode.PARTLY_CLOUDY -> if (day) wCode?.image + "day"
+        else wCode?.image + "night"
+
+        else -> wCode?.image
+    }
+    val context = LocalContext.current
+    val wIcon = context.resources.getIdentifier(
+        wImage, "drawable",
+        context.packageName
+    )
+    if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        LandscapeWeatherUI(
+            wIcon,
+            latitude,
+            longitude,
+            temperature,
+            windSpeed,
+            windDirection,
+            weathercode,
+            seaLevelPressure,
+            time,
+            onLatitudeChange = { newValue ->
+                newValue.toFloatOrNull()?.let {
+                    weatherViewModel.updateLatitude(it)
+                }
+            },
+            onLongitudeChange = { newValue ->
+                newValue.toFloatOrNull()?.let {
+                    weatherViewModel.updateLongitude(it)
+                }
+            },
+            onUpdateButtonClick = {
+                weatherViewModel.fetchWeather()
+            }
+        )
+    } else {
+        PortraitWeatherUI(
+            wIcon,
+            latitude,
+            longitude,
+            temperature,
+            windSpeed,
+            windDirection,
+            weathercode,
+            seaLevelPressure,
+            time,
+            onLatitudeChange = { newValue ->
+                newValue.toFloatOrNull()?.let {
+                    weatherViewModel.updateLatitude(it)
+                }
+            },
+            onLongitudeChange = { newValue ->
+                newValue.toFloatOrNull()?.let {
+                    weatherViewModel.updateLongitude(it)
+                }
+            },
+            onUpdateButtonClick = {
+                weatherViewModel.fetchWeather()
+            }
+        )
+    }
+}
+
+@Composable
+fun PortraitWeatherUI(
+    wIcon: Int,
+    latitude: Float,
+    longitude: Float,
+    temperature: Float,
+    windSpeed: Float,
+    windDirection: Int,
+    weathercode: Int,
+    seaLevelPressure: Float,
+    time: String,
+    onLatitudeChange: (String) -> Unit,
+    onLongitudeChange: (String) -> Unit,
+    onUpdateButtonClick: () -> Unit,
+) {
+    Column() {
+        Row() {
+            Card() {
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text("Latitude") }
+                )
+            }
+        }
+    }
+    Column() {
+        Row() {
+            Card() {
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text("Longitude") }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LandscapeWeatherUI(
+    wIcon: Int,
+    latitude: Float,
+    longitude: Float,
+    temperature: Float,
+    windSpeed: Float,
+    windDirection: Int,
+    weathercode: Int,
+    seaLevelPressure: Float,
+    time: String,
+    onLatitudeChange: (String) -> Unit,
+    onLongitudeChange: (String) -> Unit,
+    onUpdateButtonClick: () -> Unit,
+) {
+// ToDo
+}
